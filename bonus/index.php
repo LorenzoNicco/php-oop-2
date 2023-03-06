7eecf0780e4d5dc0249d468e30dc9dc6a2ca5643
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-03-06 22:51:39 +0100

    Add test per reindirizzamento pagina

--- bonus/index.php
+++ bonus/index.php
@@ -1,25 +1,52 @@
 <?php
-    require_once __DIR__ .'/classes/product.php';
-    require_once __DIR__ .'/classes/petDog.php';
-    require_once __DIR__ .'/classes/petCat.php';
+    session_start();
+    // require_once __DIR__ .'/classes/product.php';
+    // require_once __DIR__ .'/classes/petDog.php';
+    // require_once __DIR__ .'/classes/petCat.php';
+    include __DIR__ .'/functions.php';
 
-    $dogBall = new Dog('cane', 'gioco', 1);
-    $dogBall->setNameAndPic();
-    $dogBall->setPrice();
+    // $dogBall = new Dog('cane', 'gioco', 1);
+    // $dogBall->setNameAndPic();
+    // $dogBall->setPrice();
 
-    $catHouse = new Cat('gatto', 'cuccia', 1);
-    $catHouse->setNameAndPic();
-    $catHouse->setPrice();
+    // $catHouse = new Cat('gatto', 'cuccia', 1);
+    // $catHouse->setNameAndPic();
+    // $catHouse->setPrice();
 
-    $dogFood = new Dog('cane', 'cibo', 1);
-    $dogFood->setNameAndPic();
-    $dogFood->setPrice();
+    // $dogFood = new Dog('cane', 'cibo', 1);
+    // $dogFood->setNameAndPic();
+    // $dogFood->setPrice();
 
-    $catFood = new Cat('gatto', 'cibo', 1);
-    $catFood->setNameAndPic();
-    $catFood->setPrice();
+    // $catFood = new Cat('gatto', 'cibo', 1);
+    // $catFood->setNameAndPic();
+    // $catFood->setPrice();
 
-    $products = [$dogBall, $catHouse, $dogFood, $catFood];
+    // $products = [$dogBall, $catHouse, $dogFood, $catFood];
+
+    // if (isset($_GET['targetInput']) && isset($_GET['typeInput']) && isset($_GET['unitsInput'])) {
+    //     $targetInput = $_GET['targetInput'];
+    //     $typeInput = $_GET['typeInput'];
+    //     $unitsInput = $_GET['unitsInput'];
+
+    //     if ($targetInput == 'cane') {
+    //         $product = new Dog('cane', $typeInput, $unitsInput);
+    //         $product->setNameAndPic();
+    //         $product->setPrice();
+
+    //         $_SESSION['product'] = $product;
+
+    //         header('Location: ./productShow.php');
+    //     }
+    //     elseif ($targetInput == 'gatto') {
+    //         $product = new Cat('gatto', $typeInput, $unitsInput);
+    //         $product->setNameAndPic();
+    //         $product->setPrice();
+
+    //         $_SESSION['product'] = $product;
+
+    //         header('Location: ./productShow.php');
+    //     }
+    // }
 ?>
 
 <!DOCTYPE html>
@@ -34,72 +61,69 @@
         <title>PHP OOP 2</title>
     </head>
     <body>
-        <!-- versione 1 -------------------------------------------------------------------------------------------->
+
+        <form action="" method="GET">
+            <div>
+                <label for="targetInput">Per quale animale fai acquisti?</label>
+                <select name="targetInput" id="targetInput">
+                    <option value="cane">Cane</option>
+                    <option value="gatto">Gatto</option>
+                </select>
+            </div>
+
+            <div>
+                <label for="typeInput">Che tipo di prodotto stai cercando?</label>
+                <select name="typeInput" id="typeInput">
+                    <option value="cibo">Cibo</option>
+                    <option value="cuccia">Cuccia</option>
+                    <option value="gioco">Gioco</option>
+                </select>
+            </div>
+
+            <div>
+                <label for="unitsInput">Di quante unità hai bisogno?</label>
+                <input type="number" name="unitsInput" id="unitsInput">
+            </div>
+
+            <button type="submit">Cerca</button>
+        </form>
+
+        <!-- card nella stessa pagina ----------------------------------------------------------->
         <!-- <div class="card" style="width: 18rem;">
-            <img src="https://www.stockvault.net/data/2016/05/19/198785/preview16.jpg" class="card-img-top" alt="...">
+            <img src=<?php echo $product->pic ?> class="card-img-top" alt="...">
 
             <div class="card-body">
                 <h5 class="card-title">
-                    <?php echo $dogBall->name; ?>
+                    <?php echo $product->name; ?>
                 </h5>
             </div>
 
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">
-                    Prodotto per: <?php echo $dogBall->target; ?>
+                    Prodotto per: 
+                    <?php 
+                        if ($product->target == 'gatto') {
+                    ?>
+                            <i class="fa-solid fa-cat"></i>
+                    <?php
+                        }
+                        elseif ($product->target == 'cane') {
+                    ?>
+                            <i class="fa-solid fa-dog"></i>
+                    <?php
+                        }
+                    ?>
                 </li>
                 <li class="list-group-item">
-                    Quantità: <?php echo $dogBall->units; ?>
+                    Tipo di prodotto: <?php echo $product->type; ?>
                 </li>
                 <li class="list-group-item">
-                    Prezzo: <?php echo $dogBall->price; ?>
+                    Quantità: <?php echo $product->units; ?>
+                </li>
+                <li class="list-group-item">
+                    Prezzo: <?php echo $product->price; ?>
                 </li>
             </ul>
         </div> -->
-
-        <!-- versione 2 ---------------------------------------------------------------------------------------->
-        <?php 
-            foreach ($products as $product) {
-
-        ?>
-            <div class="card" style="width: 18rem;">
-                <img src=<?php echo $product->pic ?> class="card-img-top" alt="...">
-
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <?php echo $product->name; ?>
-                    </h5>
-                </div>
-
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
-                        Prodotto per: 
-                        <?php 
-                            if ($product->target == 'gatto') {
-                        ?>
-                                <i class="fa-solid fa-cat"></i>
-                        <?php
-                            }
-                            elseif ($product->target == 'cane') {
-                        ?>
-                                <i class="fa-solid fa-dog"></i>
-                        <?php
-                            }
-                        ?>
-                    </li>
-                    <li class="list-group-item">
-                        Tipo di prodotto: <?php echo $product->type; ?>
-                    </li>
-                    <li class="list-group-item">
-                        Quantità: <?php echo $product->units; ?>
-                    </li>
-                    <li class="list-group-item">
-                        Prezzo: <?php echo $product->price; ?>
-                    </li>
-                </ul>
-            </div>
-        <?php
-            }
-        ?>
     </body>
 </html>
